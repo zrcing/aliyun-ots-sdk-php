0cfcafb647b6ab11cbaebb492b2c52fd34ea5c91
Gary <liaogling@gmail.com>
2017-03-24 14:52:02 +0800

    Clean

--- src/examples/ExampleConfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('EXAMPLE_END_POINT', 'http://llspace.cn-beijing.ots.aliyuncs.com');
-define('EXAMPLE_ACCESS_KEY_ID', 'qccKPGgQndLGrOba');
-define('EXAMPLE_ACCESS_KEY_SECRET', '8aEjZKkG25UrtcmI0kP9kNLHEQ0zHJ');
-define('EXAMPLE_INSTANCE_NAME', 'llspaceLocal');
